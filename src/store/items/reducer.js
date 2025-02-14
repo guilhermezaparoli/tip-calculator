86d8689703ca05913ee612557e3c6c7cc47fcbcd
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-02-13 21:33:38 -0300

    adding MenuItemContainer

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,4 +1,4 @@
-import { ITEM_ADDED } from "./actions";
+import { ITEM_ADDED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED, ITEM_REMOVED } from "./actions";
 
 let id = 1;
 
@@ -8,12 +8,37 @@ export const initialItems = [
 ];
 
 export const reducer = (state = initialItems, action) => {
-
+console.log(state, action)
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload }
 
     return [...state, item]
   }
+
+  if (action.type === ITEM_REMOVED) {
+    const newArray = state.filter((item) => item.uuid !== action.payload.uuid)
+
+    return newArray
+  }
+
+  if (action.type === ITEM_PRICE_UPDATED) {
+    return state.map((item) => {
+      if (item.uuid === action.payload.uuid) {
+        return { ...item, price: action.payload.price }
+      }
+      return item
+    })
+  }
+
+  if (action.type === ITEM_QUANTITY_UPDATED) {
+    return state.map((item) => {
+      if (item.uuid === action.payload.uuid) {
+        return { ...item, quantity: action.payload.quantity }
+      }
+      return item
+    })
+  }
+
 
 
   return state;
